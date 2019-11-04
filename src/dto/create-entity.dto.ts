2e276de023ad84354794e8ffd8c1239c8cf9ab14
Alejandro Paez <alepdlb@gmail.com>
2019-11-04 15:32:54 +0100

    Remove class transfomer decorators

--- src/dto/create-entity.dto.ts
+++ src/dto/create-entity.dto.ts
@@ -1,18 +1,13 @@
-import { Exclude, Expose } from 'class-transformer';
 import { IsBoolean, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
-@Exclude()
 export class CreateEntityDto {
-  @Expose()
   @IsNotEmpty()
   readonly field1: string;
 
-  @Expose()
   @IsNumber()
   @IsOptional()
   readonly field2?: number;
 
-  @Expose()
   @IsBoolean()
   @IsOptional()
   readonly field3?: boolean;
